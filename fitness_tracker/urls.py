86457b95d1cd2f2aab47ecf5c191d353ee74df33
RZeil24 <rzeilinger@unomaha.edu>
2024-10-21 18:55:51 -0500

    working on code

--- fitness_tracker/urls.py
+++ fitness_tracker/urls.py
@@ -17,12 +17,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
+from tracker import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('tracker.urls')),
     path('login/', auth_views.LoginView.as_view(template_name='tracker/login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('logout/', auth_views.LogoutView.as_view(next_page='workout_list'), name='logout'),
 #    path('signup/', signup, name='signup'),
-
+    path('', views.workout_list, name='workout_list'),
 ]
--- tracker/urls.py
+++ tracker/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
     path('', views.workout_list, name='workout_list'),
+    path('login/', auth_views.LoginView.as_view(template_name='tracker/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(next_page='/'), name='logout'),
+    path('signup/', views.signup, name='signup'),
     path('add/', views.add_workout, name='add_workout'),
  #   path('signup/', signup, name='signup'),
 ]
