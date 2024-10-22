20e3cf8baf5fec8aee4cdcc5dd9a7fe45a07def7
RZeil24 <rzeilinger@unomaha.edu>
2024-10-21 19:25:25 -0500

    working on login and logout

--- tracker/urls.py
+++ tracker/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
+from django.views.generic import RedirectView
 from . import views
 
 urlpatterns = [
-    path('', views.workout_list, name='workout_list'),
+    path('', RedirectView.as_view(url='/login/', permanent=False), name='home'),  # Redirect to login
     path('login/', auth_views.LoginView.as_view(template_name='tracker/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(next_page='/'), name='logout'),
-    path('signup/', views.signup, name='signup'),
+#    path('signup/', views.signup, name='signup'),
     path('add/', views.add_workout, name='add_workout'),
- #   path('signup/', signup, name='signup'),
+    path('workouts/', views.workout_list, name='workout_list'),
 ]
