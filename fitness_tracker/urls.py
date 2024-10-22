e42dd2c00ab2abf1abf04e3c5201b03a1a467d97
RZeil24 <rzeilinger@unomaha.edu>
2024-10-21 19:39:10 -0500

    working on login and logout

--- fitness_tracker/urls.py
+++ fitness_tracker/urls.py
@@ -22,9 +22,9 @@ from tracker import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('tracker.urls')),
-   path('login/', auth_views.LoginView.as_view(template_name='tracker/login.html'), name='login'),
+    path('login/', auth_views.LoginView.as_view(template_name='tracker/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(next_page='workout_list'), name='logout'),
 #    path('signup/', signup, name='signup'),
-    path('', views.workout_list, name='workout_list'),
+    path('workouts/', views.workout_list, name='workout_list'),
 
 ]
--- tracker/urls.py
+++ tracker/urls.py
@@ -1,10 +1,10 @@
-from django.urls import path
+from django.urls import path, include
 from django.contrib.auth import views as auth_views
 from django.views.generic import RedirectView
 from . import views
 
 urlpatterns = [
-    path('', RedirectView.as_view(url='/login/', permanent=False), name='home'),  # Redirect to login
+    path('', RedirectView.as_view(url='/login/', permanent=False), name='home'),
     path('login/', auth_views.LoginView.as_view(template_name='tracker/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(next_page='/'), name='logout'),
 #    path('signup/', views.signup, name='signup'),
