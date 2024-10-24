4e028e934c71b2026e1c3cbc8d72ee8a82330d77
David Cook <dcook12@huskers.unl.edu>
2024-10-24 15:05:24 -0500

    Take in first and last name during signup

--- tracker/forms.py
+++ tracker/forms.py
@@ -11,14 +11,18 @@ class WorkoutForm(forms.ModelForm):
 
 class CustomUserCreationForm(UserCreationForm):  # Add this new form
     email = forms.EmailField(required=True)
+    first_name = forms.CharField(max_length=30, required=True)
+    last_name = forms.CharField(max_length=30, required=True)
 
     class Meta:
         model = User
-        fields = ['email', 'username', 'password1', 'password2']
+        fields = ['first_name', 'last_name', 'email', 'username', 'password1', 'password2']
 
     def save(self, commit=True):
         user = super().save(commit=False)
         user.email = self.cleaned_data['email']
+        user.first_name = self.cleaned_data['first_name']
+        user.last_name = self.cleaned_data['last_name']
         if commit:
             user.save()
         return user
