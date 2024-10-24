b6b8c3c8b27ce4660f7bf10f398fc8ab5256abe0
RZeil24 <rzeilinger@unomaha.edu>
2024-10-24 17:22:43 -0500

    updating code

--- fitness_tracker/settings.py
+++ fitness_tracker/settings.py
@@ -133,7 +133,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 #SMTP Config
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'libararyhost2024@gmail.com'
+EMAIL_HOST_PASSWORD = 'firw srkc qdet mgjl'  # Your email password
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = '<EMAIL>'
-EMAIL_HOST_PASSWORD = '<PASSWORD>'
+SERVER_EMAIL = 'root@gmail.com'
+#    'root@gmail.com'
