3e994fea2ebfe96a4509c61d0c1c8461b43c4dc4
Jormungandr115 <shthsm00@gmail.com>
2024-11-16 21:50:35 -0600

    workout changes
    this includes changes to the standard and custom workout

--- tracker/views.py
+++ tracker/views.py
@@ -1,40 +1,50 @@
 from django.shortcuts import render, redirect
 from .models import Workout, Category
-from .forms import CombinedWorkoutForm, ProfileUpdateForm, CustomUserCreationForm  # Import ProfileUpdateForm
+from .forms import CombinedWorkoutForm, CustomWorkoutForm, ProfileUpdateForm, CustomUserCreationForm
 from django.contrib.auth.decorators import login_required
 from django.http import JsonResponse
-from django.template.loader import render_to_string
 from .models import Exercise
-from django.shortcuts import render
-from django.db.models import Count
-from datetime import datetime, timedelta
-from django.db.models.functions import TruncDate
-
 
 @login_required
 def workout_list(request):
     """
-     View to display all workouts, ordered by date descending.
-     Requires user to be logged in.
-     """
+    View to display all workouts, ordered by date descending.
+    Requires user to be logged in.
+    """
     workouts = Workout.objects.all().order_by('-date')
     return render(request, 'tracker/workout_list.html', {'workouts': workouts})
 
 # View to add a new workout
 @login_required
 def add_workout(request):
+    """
+    Handles adding a standard workout and custom workout.
+    """
     # Number of visits to this view, as counted in the session variable.
     num_visits = request.session.get('num_visits', 0)
     request.session['num_visits'] = num_visits + 1
 
     if request.method == 'POST':
-        form = CombinedWorkoutForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect('workout_list')
+        # Check if adding a standard workout
+        if 'standard_workout' in request.POST:
+            standard_form = CombinedWorkoutForm(request.POST)
+            if standard_form.is_valid():
+                standard_form.save()
+                return redirect('workout_list')
+        # Check if adding a custom workout
+        elif 'custom_workout' in request.POST:
+            custom_form = CustomWorkoutForm(request.POST)
+            if custom_form.is_valid():
+                custom_form.save()
+                return redirect('workout_list')
     else:
-        form = CombinedWorkoutForm()
-    return render(request, 'tracker/add_workout.html', {'form': form})
+        standard_form = CombinedWorkoutForm()
+        custom_form = CustomWorkoutForm()
+
+    return render(request, 'tracker/add_workout.html', {
+        'standard_form': standard_form,
+        'custom_form': custom_form,
+    })
 
 def signup(request):
     if request.method == 'POST':
@@ -50,12 +60,12 @@ def signup(request):
 @login_required
 def profile(request):
     if request.method == 'POST':
-        form = ProfileUpdateForm(request.POST, instance=request.user)  # Use ProfileUpdateForm instead of UserChangeForm
+        form = ProfileUpdateForm(request.POST, instance=request.user)
         if form.is_valid():
             form.save()
-            return redirect('profile')  # Redirect back to the profile page after saving
+            return redirect('profile')
     else:
-        form = ProfileUpdateForm(instance=request.user)  # Use ProfileUpdateForm instead of UserChangeForm
+        form = ProfileUpdateForm(instance=request.user)
 
     return render(request, 'tracker/profile.html', {'form': form})
 
@@ -73,7 +83,6 @@ def load_exercises(request):
     exercises = Exercise.objects.filter(category_id=category_id)
     exercise_list = [{"id": exercise.id, "name": exercise.name} for exercise in exercises]
     return JsonResponse(exercise_list, safe=False)
-
 
 @login_required
 def exercise_history(request):
