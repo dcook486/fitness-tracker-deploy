4af4e70f30f29dcf5a5630466440808571c46998
RZeil24 <rzeilinger@unomaha.edu>
2024-12-12 17:51:33 -0600

    adding unit Test

--- tests/unitTest_login.py
+++ tests/unitTest_login.py
@@ -4,7 +4,9 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.common.by import By
+
 class ll_ATS(unittest.TestCase):
+
     def setUp(self):
         self.driver = webdriver.Chrome()
 
@@ -14,6 +16,7 @@ class ll_ATS(unittest.TestCase):
         driver = self.driver
         driver.maximize_window()
         driver.get("http://127.0.0.1:8000/admin")
+
         elem = driver.find_element(By.ID,"id_username")
         elem.send_keys(user)
         elem = driver.find_element(By.ID,"id_password")
@@ -22,20 +25,20 @@ class ll_ATS(unittest.TestCase):
         elem.send_keys(Keys.RETURN)
         driver.get("http://127.0.0.1:8000")
         time.sleep(3)
-    try:
-        # attempt to find the 'Logout' button - if found, logged in
-        elem = driver.find_element(By.ID, "Login")
-        driver.close()
-        self.fail("Login Failed - user may not exist")
-        assert False
 
-    except NoSuchElementException:
-        driver.close()
-        assert True
+        try:
+            # attempt to find the 'Logout' button - if found, logged in
+            elem = driver.find_element(By.ID, "Login")
+            driver.close()
+            self.fail("Login Failed - user may not exist")
+            assert False
+
+        except NoSuchElementException:
+            driver.close()
+            assert True
         time.sleep(3)
-
     def tearDown(self):
-    self.driver.quit()
+        self.driver.quit()
 
 if __name__ == "__main__":
     unittest.main(warnings='ignore')
